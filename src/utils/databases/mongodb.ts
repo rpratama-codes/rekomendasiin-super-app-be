27183cb56c82ee34e3260fbe5071758eee431714
rpratama-codes <35159027+rpratama-codes@users.noreply.github.com>
2025-10-30 21:33:56 +0700

    feat: add mongodb db connection

---
+++ src/utils/databases/mongodb.ts
@@ -0,0 +1,32 @@
+// Using ES6 imports
+import mongoose from 'mongoose';
+import { logger } from '../logger/winston.ts';
+
+/**
+ * I Am using `mongoose.createConnection` because i want to share MongoClient instance.
+ * So this way can make MongoClient use to another purpose. Not only Mongose.
+ *
+ * Because :	`mongoose.createConnection` Returns the MongoDB driver MongoClient
+ * 				instance that this connection uses to talk to MongoDB.
+ *
+ * Rizqi Pratama
+ * Date : 2025-10-30
+ */
+
+const connection = mongoose.createConnection(process.env.MONGO_URI as string, {
+	appName: `rekomendasiin-${Date.now()}`,
+});
+
+const mongoDbClient = connection.getClient();
+
+const instance = async () => {
+	await mongoDbClient.connect();
+};
+
+instance().catch((e: unknown) => {
+	if (e instanceof Error) {
+		logger.error(e);
+	}
+});
+
+export { mongoDbClient, mongoose };
