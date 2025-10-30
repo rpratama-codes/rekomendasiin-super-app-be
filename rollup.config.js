0d9f9bf0fa61335a1eb8da09933a47a6b5cac8e9
rpratama-codes <35159027+rpratama-codes@users.noreply.github.com>
2025-10-30 21:32:55 +0700

    chore: bundle dependency into app

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,12 @@
-import terser from '@rollup/plugin-terser';
+/** biome-ignore-all assist/source/organizeImports: <this is a config file so doesn't need to be lint> */
+
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import json from '@rollup/plugin-json';
 import copy from 'rollup-plugin-copy';
+
 // rollup.config.js
 /**
  * @type {import('rollup').RollupOptions}
@@ -14,6 +20,9 @@ export default {
 	},
 	plugins: [
 		typescript(),
+		commonjs(),
+		json(),
+		nodeResolve({preferBuiltins: true}),
 		terser(),
 		copy({
 			targets: [
@@ -28,5 +37,6 @@ export default {
 				},
 			],
 		}),
+
 	],
 };
