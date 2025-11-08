397ef6646f422eadee89ad2e70f208df6c021140
rpratama-codes <35159027+rpratama-codes@users.noreply.github.com>
2025-11-08 07:37:20 +0700

    chore: much thing happened

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
 	ssr: {
@@ -6,12 +7,29 @@ export default defineConfig({
 		 * Still find out to include prisma and argon2 into bundle.
 		 */
 		noExternal: true,
-		external: ["@prisma/client", "argon2"],
+		external: ["@prisma/client", "@prisma/adapter-pg", "argon2"],
 	},
 	build: {
 		outDir: "./dist",
 		ssr: "./src/index.ts",
 	},
-	plugins: [],
+	plugins: [
+		viteStaticCopy({
+			targets: [
+				{
+					src: "package.json",
+					dest: "",
+				},
+				{
+					src: "pnpm-lock.yaml",
+					dest: "",
+				},
+				{
+					src: "src/services/prisma/schema.prisma",
+					dest: "",
+				},
+			],
+		}),
+	],
 	resolve: {},
 });
