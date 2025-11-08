7889d6300d2b0cacd13c4f1b692f20004998e8af
rpratama-codes <35159027+rpratama-codes@users.noreply.github.com>
2025-11-08 07:49:13 +0700

    fix: change middleware order

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 import { errorHandlerMiddleware } from './middleware/error-handler.js';
 import { loggerMiddleware } from './middleware/logger.js';
-import { authRouteV1 } from './routes/auth-v1.route.js';
+import { authRouteV1 } from './routes/auth/auth-v1.route.js';
 import { indexRoute } from './routes/index.route.js';
 import { storeFrontRoute } from './routes/store-front.route.js';
 import { logger } from './utils/logger/winston.js';
@@ -11,15 +11,20 @@ const app = express();
 const router = express.Router();
 const port = process.env.APP_PORT;
 
-// If your app is served through a proxy
-// trust the proxy to allow authjs to read the `X-Forwarded-*` headers
+/**
+ * Config for auth.js to allow read the X-Forwarded-*` headers
+ */
 app.set('trust proxy', true);
 
-app.use(loggerMiddleware);
+/**
+ * THE ORDER IS MATTER!!!
+ */
+
 app.use(router);
-app.use(errorHandlerMiddleware);
 app.use(express.json());
+app.use(loggerMiddleware);
 app.use('/api', router);
+app.use(errorHandlerMiddleware);
 
 router.use(indexRoute);
 router.use(storeFrontRoute);
