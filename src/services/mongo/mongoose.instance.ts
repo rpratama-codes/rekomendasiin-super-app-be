75cf6d9cedc423a13a3736ce77387e73006a6a4a
rpratama-codes <35159027+rpratama-codes@users.noreply.github.com>
2025-11-26 11:27:10 +0700

    fix: fixing uncompatible mongodb version

--- src/services/mongo/mongoose.instance.ts
+++ src/services/mongo/mongoose.instance.ts
@@ -5,7 +5,8 @@ const mongoDbClient = new MongoService();
 
 /**
  * New issue for mongoose, tell them to add support mongodb client v7.0.0
+ * biome-ignore lint/suspicious/noExplicitAny: incompability type.
  */
-mongoose.createConnection().setClient(mongoDbClient);
+mongoose.createConnection().setClient(mongoDbClient as any);
 
 export { mongoose };
